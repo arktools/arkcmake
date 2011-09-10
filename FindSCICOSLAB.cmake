61b09dddfd804f93612f76b1bba54567fc12a209
Lenna Peterson <arklenna@gmail.com>
2011-09-10 16:42:47 -0400

    Fixed contrib dir for Darwin.

--- FindSCICOSLAB.cmake
+++ FindSCICOSLAB.cmake
@@ -16,7 +16,7 @@ if (APPLE)
         ERROR_VARIABLE ERROR_MESSAGE
         OUTPUT_STRIP_TRAILING_WHITESPACE)
     if (RESULT) 
-        MESSAGE(FATAL_ERROR "Could not locate ScicosLabGtk.app - ${ERROR_MESSAGE}")
+        MESSAGE(FATAL_ERROR "Could not locate 'ScicosLabGtk.app' - ${ERROR_MESSAGE}")
     endif (RESULT)
     execute_process(COMMAND find ${SCICOS_APP_BUNDLE} -name routines
         COMMAND head -1
@@ -25,7 +25,16 @@ if (APPLE)
         ERROR_VARIABLE ERROR_MESSAGE
         OUTPUT_STRIP_TRAILING_WHITESPACE)
     if (RESULT) 
-        MESSAGE(FATAL_ERROR "Could not locate scicos_block4.h in ScicosLabGtk.app - ${ERROR_MESSAGE}")
+        MESSAGE(FATAL_ERROR "Could not locate 'scicos_block4.h' in ScicosLabGtk.app - ${ERROR_MESSAGE}")
+    endif (RESULT)  
+    execute_process(COMMAND find ${SCICOS_APP_BUNDLE} -name contrib 
+        COMMAND head -1
+        RESULT_VARIABLE RESULT
+        OUTPUT_VARIABLE SCICOSLAB_GUESS_CONTRIB_DIRS
+        ERROR_VARIABLE ERROR_MESSAGE
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if (RESULT) 
+        MESSAGE(FATAL_ERROR "Could not locate 'loader.sce' in ScicosLabGtk.app - ${ERROR_MESSAGE}")
     endif (RESULT)  
 elseif(UNIX)
     set(SCICOSLAB_GUESS_INCLUDE_DIRS
@@ -33,6 +42,11 @@ elseif(UNIX)
         /usr/lib/scicoslab-gtk-4.4/routines
         /usr/lib/scicoslab-gtk-4.4.1/routines
         )
+	set(SCICOSLAB_GUESS_CONTRIB_DIRS
+		/usr/lib/scicoslab-gtk-4.4b7/contrib
+		/usr/lib/scicoslab-gtk-4.4/contrib
+		/usr/lib/scicoslab-gtk-4.4.1/contrib
+	)
 elseif(WIN32)
     message(FATAL_ERROR "scicoslab cmake find module doesn't work for windows")
 endif()
@@ -47,11 +61,9 @@ find_path(SCICOSLAB_INCLUDE_DIR
 # Contrib dir
 find_path(SCICOSLAB_CONTRIB_DIR
   NAMES loader.sce
-  PATHS
-    /usr/lib/scicoslab-gtk-4.4b7/contrib
-    /usr/lib/scicoslab-gtk-4.4/contrib
-    /usr/lib/scicoslab-gtk-4.4.1/contrib
+  PATHS ${SCICOSLAB_GUESS_CONTRIB_DIRS}
 )
+message(STATUS "contrib dir ${SCICOSLAB_CONTRIB_DIR}")
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
