23122eb328399651debdc27c01612b49b98d900b
James Goppert <james.goppert@gmail.com>
2012-01-30 17:53:15 -0500

    Added arkhangar find or build.

--- BuildOpenSceneGraph.cmake
+++ BuildOpenSceneGraph.cmake
@@ -10,30 +10,27 @@ include(MacroCommonPaths)
 
 MacroCommonPaths(OPENSCENEGRAPH)
 
-macro(find_or_build_openscenegraph TAG EP_BASE_DIR EP_INSTALL_PREFIX EP_DATADIR)
-    find_package(OPENSCENEGRAPH ${TAG})
-    if(NOT OPENSCENEGRAPH_FOUND)
-        ExternalProject_Add(openscenegraph
-            SVN_REPOSITORY "http://www.openscenegraph.org/svn/osg/OpenSceneGraph/tags/OpenSceneGraph-${TAG}"
-            UPDATE_COMMAND ""
-            INSTALL_DIR ${EP_BASE_DIR}/${EP_INSTALL_PREFIX}
-            CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${EP_INSTALL_PREFIX}
-            INSTALL_COMMAND make DESTDIR=${EP_BASE_DIR} install
-           )
-        set(OPENSCENEGRAPH_INCLUDE_DIRS ${EP_BASE_DIR}/${EP_INSTALL_PREFIX}/include)
-        set(OPENSCENEGRAPH_DATA_DIR ${EP_DATADIR}/openscenegraph/data)
-        # static lib prefix
-        if(WIN32)
-            set(STATIC_LIB_PREFIX "")
-        elseif(APPLE)
-            set(STATIC_LIB_PREFIX "lib")
-        elseif(UNIX)
-            set(STATIC_LIB_PREFIX "lib")
-        else()
-            message(FATAL_ERROR "unknown operating system")
-        endif()
-        set(OPENSCENEGRAPH_LIBRARIES 
-            )
-        set(OPENSCENEGRAPH_FOUND TRUE)
+macro(build_openscenegraph TAG EP_BASE_DIR EP_INSTALL_PREFIX EP_DATADIR)
+    ExternalProject_Add(openscenegraph
+        SVN_REPOSITORY "http://www.openscenegraph.org/svn/osg/OpenSceneGraph/tags/OpenSceneGraph-${TAG}"
+        UPDATE_COMMAND ""
+        INSTALL_DIR ${EP_BASE_DIR}/${EP_INSTALL_PREFIX}
+        CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${EP_INSTALL_PREFIX}
+        INSTALL_COMMAND make DESTDIR=${EP_BASE_DIR} install
+       )
+    set(OPENSCENEGRAPH_INCLUDE_DIRS ${EP_BASE_DIR}/${EP_INSTALL_PREFIX}/include)
+    set(OPENSCENEGRAPH_DATA_DIR ${EP_DATADIR}/openscenegraph/data)
+    # static lib prefix
+    if(WIN32)
+        set(STATIC_LIB_PREFIX "")
+    elseif(APPLE)
+        set(STATIC_LIB_PREFIX "lib")
+    elseif(UNIX)
+        set(STATIC_LIB_PREFIX "lib")
+    else()
+        message(FATAL_ERROR "unknown operating system")
     endif()
+    set(OPENSCENEGRAPH_LIBRARIES 
+        )
+    set(OPENSCENEGRAPH_FOUND TRUE)
 endmacro()
