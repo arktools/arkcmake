16f3105ea034157dc2313323d4bb93f36de227cf
James Goppert <james.goppert@gmail.com>
2012-02-15 11:06:50 -0500

    Modified gitignore.

--- gitignore.ex
+++ gitignore.ex
@@ -1,4 +1,12 @@
-*.swp
-*.pyc
-.DS_Store
-arkcmake
+# only keep relevant arkcmake files
+arkcmake/*
+!arkcmake/DefineCMakeDefaults.cmake
+!arkcmake/DefinePlatformDefaults.cmake
+!arkcmake/DefineCompilerFlags.cmake
+!arkcmake/DefineInstallationPaths.cmake
+!arkcmake/MacroEnsureOutOfSourceBuild.cmake
+!arkcmake/MacroCheckCCompilerFlagSSP.cmake
+!arkcmake/CommonSetup.cmake
+!arkcmake/updateArkcmake.py
+!arkcmake/autobuild.py
+!arkcmake/get_build_path.py
