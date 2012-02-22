2c427087b2640a121d45d4ac753ed6cac1bfe597
James Goppert <james.goppert@gmail.com>
2012-02-21 23:30:14 -0500

    Removed common setup.

--- gitignore.ex
+++ gitignore.ex
@@ -6,7 +6,6 @@ arkcmake/*
 !arkcmake/DefineInstallationPaths.cmake
 !arkcmake/MacroEnsureOutOfSourceBuild.cmake
 !arkcmake/MacroCheckCCompilerFlagSSP.cmake
-!arkcmake/CommonSetup.cmake
 !arkcmake/updateArkcmake.py
 !arkcmake/autobuild.py
 !arkcmake/get_build_path.py
